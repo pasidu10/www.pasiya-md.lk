75eb5eccffcf784b8bccace1c4d9dc4570007c06
pasidu10 <punnyanamda.thero@gmail.com>
2025-11-10 22:39:22 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-const GEMINI_KEY = "AQ.Ab8RN6JT14b4cqUw7POMJ_BX-fRiWpTvAZ9AJ-x0LW3UhMxAZg";
+const GEMINI_KEY = "AIzaSyDq2Wa7puYSVYkaQY0TI_JYf2cWpOVNkiU";
 
 async function askGemini(question) {
   const response = await fetch(
